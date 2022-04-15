354b074adfc2e44c0078f99037f7aa5098d4a48c
Marcelo Ramos <mramosecua@gmail.com>
2022-04-15 11:43:30 -0400

    added random

---
+++ src/helpers/getPokemonOptions.js
@@ -0,0 +1,16 @@
+const getPokemons = () => {
+  const pokemonsArr = Array.from(Array(650))
+
+  return pokemonsArr.map((_, index) => index + 1)
+}
+
+const getPokemonOptions = () => {
+  const mixedPokemons = getPokemons().sort(() => Math.random() - 0.5)
+  getPokemonNames(mixedPokemons.splice(0, 4))
+}
+
+const getPokemonNames = (pokemons = []) => {
+
+}
+
+export default getPokemonOptions
