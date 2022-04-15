2113d2b4538302af08b8ed4b16f89031d977796f
Marcelo Ramos <mramosecua@gmail.com>
2022-04-15 12:53:05 -0400

    added showPokemon

--- src/pages/PokemonPage.vue
+++ src/pages/PokemonPage.vue
@@ -9,6 +9,10 @@
         @selection="checkAnswer"
       />
 
+      <template v-if="showAnswer">
+        <h2 class="fade-in">{{ message }}</h2>
+        <button @click="newGame">Nuevo Juego</button>
+      </template>
     </div>
 
   </div>
@@ -26,7 +30,9 @@ export default {
     return {
       pokemonArr: [],
       pokemon: null,
-      showPokemon: false
+      showPokemon: false,
+      showAnswer: false,
+      message: ''
     }
   },
   methods: {
@@ -36,8 +42,22 @@ export default {
       const randomInt = Math.floor(Math.random() * 4)
       this.pokemon = this.pokemonArr[randomInt]
     },
-    checkAnswer (pokemonId) {
-      this.showPokemon = !!pokemonId
+    checkAnswer (selectedId) {
+      this.showPokemon = true
+      this.showAnswer = true
+
+      if (selectedId === this.pokemon.id) {
+        this.message = `Correcto!, ${this.pokemon.name}`
+      } else {
+        this.message = `Oops, era  ${this.pokemon.name}`
+      }
+    },
+    newGame () {
+      this.showPokemon = false
+      this.showAnswer = false
+      this.pokemonArr = []
+      this.pokemon = null
+      this.mixPokemonArray()
     }
   },
   mounted () {
