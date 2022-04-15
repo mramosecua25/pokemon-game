dbf13bf22c275efe932a4b05591e27a10fb29004
Marcelo Ramos <mramosecua@gmail.com>
2022-04-15 11:10:42 -0400

    added structure pokemon

--- src/pages/PokemonPage.vue
+++ src/pages/PokemonPage.vue
@@ -1,9 +1,16 @@
 <template>
-  <h1>Quien es este Pokemon?</h1>
+  <div>
+    <h1>Quien es este Pokemon?</h1>
+    <PokemonPicture />
+    <PokemonOptions />
+  </div>
 </template>
 
 <script>
-export default {
+import PokemonOptions from '@/components/PokemonOptions.vue'
+import PokemonPicture from '@/components/PokemonPicture.vue'
 
+export default {
+  components: { PokemonOptions, PokemonPicture }
 }
 </script>
